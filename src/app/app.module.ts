238c114a6821f80903d2c7c43c0505d90dad9d69
Joshua Williamson <jlwilliamson95@gmail.com>
2020-02-26 13:15:16 -0700

    removed test folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { GaugeTestComponent } from './gauge-test/gauge-test.component';
 import { GaugeModule } from 'angular-gauge';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GaugeTestComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
